06de053016c016a7af07b8c4f1ad646917cd7869
GesangPJ <g.paudra@gmail.com>
2020-12-30 09:57:20 +0700

    First Build
    First Build using Visual Studio 2019

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CPP-ConvertRomanNumber
-A Program to convert numbers to Roman numbers
+A Program to convert numbers to Roman numbers using C++ Language, build in Visual Studio 2019
---
+++ romawi/romawi.cpp
@@ -0,0 +1,82 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	double angka;
+	cout << "Masukkan Angka: ";
+	cin >> angka;
+	if (angka < 1 || angka>3999)
+		cout << "Angka Tidak Boleh Lebih Dari 3999!!";
+	else {
+		while (angka >= 1000) {
+			cout << "M";
+			angka -= 1000;
+		}
+		if (angka >= 500) {
+			if (angka > 500) {
+				if (angka >= 900) {
+					cout << "CM";
+					angka -= 900;
+				}
+				else {
+					cout << "D";
+					angka -= 500;
+				}
+			}
+		}
+
+		while (angka >= 100) {
+			if (angka >= 400) {
+				cout << "CD";
+				angka -= 400;
+			}
+			else {
+				angka -= 100;
+			}
+		}
+		if (angka >= 50) {
+			if (angka >= 90) {
+				cout << "XC";
+				angka -= 90;
+			}
+			else {
+				cout << "L";
+				angka -= 50;
+			}
+		}
+
+		while (angka >= 10) {
+			if (angka >= 40) {
+				cout << "XL";
+				angka -= 40;
+			}
+			else {
+				cout << "X";
+				angka -= 10;
+			}
+		}
+		if (angka >= 5) {
+			if (angka == 9) {
+				cout << "IX";
+				angka -= 9;
+			}
+			else {
+				cout << "V";
+				angka -= 5;
+			}
+		}
+
+		while (angka >= 1) {
+			if (angka == 4) {
+				cout << "IV";
+				angka -= 4;
+			}
+			else {
+				cout << "I";
+				angka -= 1;
+			}
+		}
+	}
+	cout << endl << endl;
+}
